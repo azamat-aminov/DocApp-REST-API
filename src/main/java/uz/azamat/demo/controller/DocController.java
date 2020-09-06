4caceb1c5ce37a7da9cf559c1bf1f80d9f6eec50
Azamat <azamatmaqsudjonovich@gmail.com>
2020-09-06 18:36:20 +0500

    controller modified

--- src/main/java/uz/azamat/demo/controller/DocController.java
+++ src/main/java/uz/azamat/demo/controller/DocController.java
@@ -1,6 +1,5 @@
 package uz.azamat.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.azamat.demo.model.Application;
@@ -32,6 +31,12 @@ public class DocController {
         return ResponseEntity.ok(allUser);
     }
 
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity updateUser(@PathVariable int id, @RequestBody User user) {
+        User updatedUser = userService.updateUser(user, id);
+        return ResponseEntity.ok(updatedUser);
+    }
+
     @PostMapping("/saveApplication")
     public int saveUser(@RequestBody Application application) {
         applicationService.saveApplication(application);
@@ -42,12 +47,6 @@ public class DocController {
     public ResponseEntity getAllApplications() {
         List<Application> allApplications = applicationService.getAllApplications();
         return ResponseEntity.ok(allApplications);
-    }
-
-    @PutMapping("/updateUser/{id}")
-    public ResponseEntity updateUser(@PathVariable int id, @RequestBody User user) {
-        User updatedUser = userService.updateUser(user, id);
-        return ResponseEntity.ok(updatedUser);
     }
 
     @PutMapping("/updateApplication/{id}")
