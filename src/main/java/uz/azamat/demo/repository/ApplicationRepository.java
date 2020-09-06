5140e35ceb5068cb38f26ee4b8b0ed6ba2d3b0a9
Azamat <azamatmaqsudjonovich@gmail.com>
2020-09-06 16:57:02 +0500

    CrudRepository changed to JpaRepository

--- src/main/java/uz/azamat/demo/repository/ApplicationRepository.java
+++ src/main/java/uz/azamat/demo/repository/ApplicationRepository.java
@@ -1,7 +1,7 @@
 package uz.azamat.demo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import uz.azamat.demo.model.Application;
 
-public interface ApplicationRepository extends CrudRepository<Application, Integer> {
+public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 }
--- src/main/java/uz/azamat/demo/repository/UserRepository.java
+++ src/main/java/uz/azamat/demo/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package uz.azamat.demo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import uz.azamat.demo.model.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findById(int id);
 }
