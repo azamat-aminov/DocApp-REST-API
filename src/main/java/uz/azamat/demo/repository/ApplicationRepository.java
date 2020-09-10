79d3be5d4fcd90f3e2bc24b8e34cae543bd5b878
Azamat <azamatmaqsudjonovich@gmail.com>
2020-09-10 16:08:06 +0500

    deleteById method modified

--- src/main/java/uz/azamat/demo/repository/ApplicationRepository.java
+++ src/main/java/uz/azamat/demo/repository/ApplicationRepository.java
@@ -1,13 +1,10 @@
 package uz.azamat.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import uz.azamat.demo.model.Application;
 
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
      Application findById(int id);
-     @Modifying
-     @Query("delete from Application a where a.id = ?1")
-     int deleteById(int id);
+
+     void deleteById(int id);
 }
